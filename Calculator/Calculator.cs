2d2dd64c134a16bd4c9d34277502bbfb575094f6
Ed <eduardo@gottabegarabi.com>
2018-08-01 15:29:29 +0100

    Created basic VS project.

---
+++ Calculator/Calculator.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    public class Calculator
+    {
+        public decimal Add(decimal x, decimal y) {
+            return x + y;
+        }
+
+        public decimal Subtract(decimal x, decimal y)
+        {
+            return x - y;
+        }
+    }
+}
