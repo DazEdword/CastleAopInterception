7de4c78f14b3e7ec5ed1c25b872b1bebe3f52522
Ed <eduardo@gottabegarabi.com>
2018-08-24 16:40:10 +0100

    Consolidated package versions.

---
+++ AopUnitTests/DemoTest.cs
@@ -0,0 +1,46 @@
+﻿using Castle.Core;
+using Castle.MicroKernel;
+using Castle.MicroKernel.ModelBuilder;
+using Castle.MicroKernel.Registration;
+using Castle.Windsor;
+using NUnit.Framework;
+using SimpleCalculator;
+using SimpleLogger;
+using System.Linq;
+
+namespace AopUnitTests
+{
+    [TestFixture]
+    public class HandleSelectorTests
+    {
+        [Test]
+        public void DemoTests()
+        {
+            using (var container = new WindsorContainer())
+            {
+                // Real Calculator registration
+                container.Kernel.ComponentModelBuilder.AddContributor(new IContributeToCalculatorFakeConstruction());
+
+                container.Register(Component.For<ILogger>().ImplementedBy<Logger>());
+                container.Register(Component.For<ICalculator>().ImplementedBy<Calculator>().Named("Calculator"));
+
+                var actual = container.Resolve<ICalculator>();
+
+                Assert.AreEqual(1000, actual.Add(0, 0));
+            }
+        }
+    }
+
+
+    public class IContributeToCalculatorFakeConstruction : IContributeComponentModelConstruction
+    {
+        public void ProcessModel(IKernel kernel, ComponentModel model)
+        {
+            if (model.Services.Contains(typeof(ICalculator)))
+            {
+                model.Implementation = typeof(CalculatorFake);
+            }
+        }
+    }
+
+}
---
+++ Calculator/Implementation/CalculatorFake.cs
@@ -0,0 +1,25 @@
+﻿namespace SimpleCalculator
+{
+    public class CalculatorFake : ICalculator
+    {
+        public decimal Add(decimal x, decimal y)
+        {
+            return 1000m;
+        }
+
+        public decimal Divide(decimal x, decimal y)
+        {
+            return 1000m;
+        }
+
+        public decimal Multiply(decimal x, decimal y)
+        {
+            return 1000m;
+        }
+
+        public decimal Subtract(decimal x, decimal y)
+        {
+            return 1000m;
+        }
+    }
+}
